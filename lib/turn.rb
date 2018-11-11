f4bc9dd4bc55e94e7de4e2e3d40eca33c984775d
Meredith Strickland <mere.strickland@gmail.com>
2018-11-11 18:58:51 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+#helper methods
+
+#tac_arr = [" ", " ", " "," ", " ", " "," ", " ", " "]
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n"  + "-----------\n"  + " #{board[3]} | #{board[4]} | #{board[5]} " + "-----------\n"  + " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move(board, index)
+  
+end
+
+def move()
+end
